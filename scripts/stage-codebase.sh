7062687a9a67b8c754c159e833567a3725f5c31d
Bill Kable <bkable@gmail.com>
2023-02-17 15:02:43 -0600

    revise codebase staging for project directory

--- scripts/stage-codebase.sh
+++ scripts/stage-codebase.sh
@@ -1,43 +1,27 @@
 #!/bin/bash
-set -e
 
-# Stage the codebase
-# This script is used to checkout the codebase to the commit that corresponds to the lesson label from the commit message.
+# This script stages the codebase for deployment.
+# It takes a single argument, the name of the directory to stage the
+# codebase into.
+#
+# NOTE: the target directory will be deleted if it already exists!
+TARGET_DIR=$1
 
-# Note this file is meant to generalize the staging of the codebase via a provided LESSON_LABEL environment variable.
-# there are no explicit parameters for this script to maximize the decoupling of the lesson content from the staging script.
+pushd $(pwd) > /dev/null
 
-# Make sure we are in the root of the repo
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-cd $SCRIPT_DIR/..
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR"/..
 
-get_sha_for_lesson_label() {
-    git log --format=format:%H --grep "$LESSON_LABEL"
-}
+if [ -z "$TARGET_DIR" ]; then
+    echo "Usage: $0 <target_dir>"
+    exit 1
+fi
 
-checkout_to_start_commit() {
-    if [[ ! -z "$LESSON_LABEL" ]]; then
-        lesson_sha=$(get_sha_for_lesson_label)
-        if [[ -z "$lesson_sha" ]]; then
-            echo "LESSON_LABEL '$LESSON_LABEL' is not mapped to commit for this workshop."
-        else
-            echo "going to do the checkout"
-            git checkout $lesson_sha
-        fi
-    else
-        echo "No lesson label provided."
-    fi
-}
+if [ -d "$TARGET_DIR" ]; then
+    rm -rf "$TARGET_DIR"
+fi
 
-set_rabbitmq_hostname() {
-    if [[ ! -z "$SESSION_NAMESPACE" ]]; then
-        if [[ ! -z "$INGRESS_DOMAIN" ]]; then
-            RABBITMQ_HOSTNAME="rmq.${SESSION_NAMESPACE}.svc.cluster.local"
-            cat ./stock-trader/src/main/resources/application.properties| sed "s/localhost/${RABBITMQ_HOSTNAME}/g" > ./stock-trader/src/main/resources/application.properties.tmp
-            mv ./stock-trader/src/main/resources/application.properties.tmp ./stock-trader/src/main/resources/application.properties
-        fi
-    fi
-}
+cd ./"$PROJECT_DIR"/..
+cp -r $(basename $PROJECT_DIR) $TARGET_DIR
 
-checkout_to_start_commit
-set_rabbitmq_hostname
+popd > /dev/null
